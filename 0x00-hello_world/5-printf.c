c745e92490027e0f6c506b5715dac3ae9cbac6a5
Markizovilla <chimitamark@gmail.com>
2022-06-02 16:05:18 -0700

    5 print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  * Description: main - Prints out a string to stdout.
@@ -7,13 +6,8 @@
  * Return: 0 if success.
  *
  */
-
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
