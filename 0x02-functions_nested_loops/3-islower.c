570f6fa180dd2c2a0867b6af86cefe54abc35736
Markizovilla <chimitamark@gmail.com>
2022-06-14 02:12:01 -0700

    _islower 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - check for lowercase character
- * @c: contain the characters to check 
+ * @c: contain the characters to check
  * Return: 1 on success or 0 otherwise
  */
 
 int _islower(int c)
 
 {
-	if (c >= 97 && c <= 122)	
+	if (c >= 97 && c <= 122)
 	{
 		return(1);
 	}
@@ -18,4 +18,4 @@ int _islower(int c)
 		return (0);
 	}
 
-}						
+}
