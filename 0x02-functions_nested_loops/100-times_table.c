2c62131a3d552437e6bd98f653d0d0528b50cb2e
Markizovilla <chimitamark@gmail.com>
2022-06-16 13:52:44 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,38 +1,69 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_to_98 - prints natural
- *
- * numbers from n to 98
- *
- * @n:integer to start by
- *
- * return: returns nothing
+ * print_times_table - prints the  times table with
+ * parameter
+ * @n: parameter
+ * Return: returns nothing
  */
 
-void print_to_98(int n)
-
+void print_times_table(int n)
 {
-	if (n >= 98)
+	int digit, mult, result;
+
+	if (n <= 15 && n >= 0)
 
 	{
-		while (n > 98)
+		for (digit = 0; digit <= n; digit++)
 
-		printf("%d, ", n--);
+		{
 
-		printf("%d\n", n);
+			_putchar('0');
 
-	}
+			for (mult = 1; mult <= n; mult++)
 
-	else
+			{
 
-	{
-		while (n < 98)
+				_putchar(',');
 
-		printf("%d, ", n++);
+				_putchar(' ');
 
-		printf("%d\n", n);
+				result = digit * mult;
+
+				if (result <= 99)
+
+					_putchar(' ');
+
+
+				if (result <= 9)
+
+					_putchar(' ');
+
+				if (result >= 100)
+
+				{
+
+					_putchar((result / 100) + '0');
+
+					_putchar((result / 10) % 10 + '0');
+
+				}
+
+				else if (result <= 99 && result >= 10)
+
+				{
+
+					_putchar((result / 10) + '0');
+
+				}
+
+				_putchar((result % 10) + '0');
+
+			}
+
+			_putchar('\n');
+
+		}
 
 	}
 
